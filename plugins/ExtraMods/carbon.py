2b594d4c1776ca8569b8e0746c005b90a782666c
Sulagna Pradhan <152047288+Sulagna-pradhan@users.noreply.github.com>
2024-02-24 16:20:13 +0000

    modified:   plugins/ExtraMods/carbon.py

--- plugins/ExtraMods/carbon.py
+++ plugins/ExtraMods/carbon.py
@@ -30,8 +30,8 @@ async def carbon_func(b, message):
     await m.edit("ᴜᴘʟᴏᴀᴅɪɴɢ..")
     await message.reply_photo(
         photo=carbon,
-        caption="**ᴍᴀᴅᴇ ʙʏ: @mkn_bots_updates**",
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("ꜱᴜᴩᴩᴏʀᴛ ᴜꜱ", url="https://t.me/mkn_bots_updates")]]),                   
+        caption="**ᴍᴀᴅᴇ ʙʏ: @Protootypes**",
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("ꜱᴜᴩᴩᴏʀᴛ ᴜꜱ", url="https://t.me/moviesscloud/5640")]]),                   
     )
     await m.delete()
     carbon.close()
